a065a08b5088b689160f01dec30ddce3eec19248
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-07-11 11:31:12 +0200

    Implement CommentsForm component

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,45 @@
-const CommentsForm = () => {
-  return <div>CommentsForm</div>;
+import { useState, useEffect, useRef } from 'react';
+
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Leave a Reply
+      </h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          name="comment"
+          placeholder="Comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="email"
+          ref={emailEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CommentsForm;
