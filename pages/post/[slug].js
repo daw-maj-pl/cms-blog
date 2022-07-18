8872c0e62796443e6201fc6433addabab75fcd3e
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-07-18 11:38:37 +0200

    Add Loader

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { getPosts, getPostDetails } from '../../services';
 import {
   PostDetail,
@@ -5,10 +6,17 @@ import {
   PostWidget,
   Author,
   Comments,
-  CommentsForm
+  CommentsForm,
+  Loader
 } from '../../components';
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
